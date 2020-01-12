2f9234fd2b7eb2f7224838644ca1545d352a93bb
dboissin <damien.boissin@web-education.net>
2020-01-12 19:31:03 +0100

    Remove duplication in df creation and charts generation

--- analyse-logs-fullpy.py
+++ analyse-logs-fullpy.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import pandas as pd
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+from dfchart.viz import generate_charts
 
 # parse nginx log file
 ranges = {}
@@ -30,41 +27,5 @@ with open('logs/access.log') as f:
         r[prefix_type + '_rt_sum'] += float(line[rtidx:rtidx+6])
         line = f.readline()
 
-# create pandas dataframe
-df = pd.DataFrame(ranges).transpose()
-
-# calculate means values
-df['static_rt_mean'] = df['static_rt_sum'] / df['static_count']
-df['upstream_rt_mean'] = df['upstream_rt_sum'] / df['upstream_count']
-df['err_rt_mean'] = df['err_rt_sum'] / df['err_count']
-
-
-df2 = df.reset_index()
-
-# création du graphique du nombre de requêtes
-fig, ax = plt.subplots(figsize=(15,7))
-ax.set_title("Nombre de requêtes groupées par plages de 10 minutes")
-ax.set_xlabel("Heure")
-ax.set_ylabel("Nombre requêtes")
-for tick in ax.get_xticklabels():
-    tick.set_rotation(45)
-
-ax.stackplot(df2['index'], df2['upstream_count'], df2['static_count'], df2['err_count'], labels=('Apps','Statics','Erreurs'))
-
-plt.legend(loc=2)
-fig.savefig('/tmp/nb_requests.png')
-
-# création du graphique de la moyenne des temps de réponses
-fig, ax = plt.subplots(figsize=(15,5))
-ax.set_title("Temps de réponse moyen par plages de 10 minutes")
-ax.set_xlabel("Heure")
-ax.set_ylabel("Temps de réponse (en secondes)")
-for tick in ax.get_xticklabels():
-    tick.set_rotation(45)
-
-ax.plot(df2['index'], df2['upstream_rt_mean'], label='Apps')
-ax.plot(df2['index'], df2['static_rt_mean'], label='Statics')
-ax.plot(df2['index'], df2['err_rt_mean'], label='Erreurs')
-
-plt.legend(loc=2)
-fig.savefig('/tmp/rt_requests.png')
+# create dataframe and generate charts
+generate_charts(ranges)
--- analyse-logs-regex.py
+++ analyse-logs-regex.py
@@ -2,11 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
-import pandas as pd
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
+from dfchart.viz import generate_charts
 
 pattern = re.compile(".*?\[([A-Za-z0-9\/:+\s]+)\]\s\"(.*?)\"\s([0-9]+)\s[0-9]+\s\".*?\"\s\".*?\"rt=([0-9]+\.[0-9]+)\s.*?urt=\"([0-9\.\-,\s]+)\"")
 
@@ -34,41 +30,5 @@ with open('logs/access.log') as f:
             print("pattern doesn't match : %s" % line)
         line = f.readline()
 
-# create pandas dataframe
-df = pd.DataFrame(ranges).transpose()
-
-# calculate means values
-df['static_rt_mean'] = df['static_rt_sum'] / df['static_count']
-df['upstream_rt_mean'] = df['upstream_rt_sum'] / df['upstream_count']
-df['err_rt_mean'] = df['err_rt_sum'] / df['err_count']
-
-
-df2 = df.reset_index()
-
-# création du graphique du nombre de requêtes
-fig, ax = plt.subplots(figsize=(15,7))
-ax.set_title("Nombre de requêtes groupées par plages de 10 minutes")
-ax.set_xlabel("Heure")
-ax.set_ylabel("Nombre requêtes")
-for tick in ax.get_xticklabels():
-    tick.set_rotation(45)
-
-ax.stackplot(df2['index'], df2['upstream_count'], df2['static_count'], df2['err_count'], labels=('Apps','Statics','Erreurs'))
-
-plt.legend(loc=2)
-fig.savefig('/tmp/nb_requests.png')
-
-# création du graphique de la moyenne des temps de réponses
-fig, ax = plt.subplots(figsize=(15,5))
-ax.set_title("Temps de réponse moyen par plages de 10 minutes")
-ax.set_xlabel("Heure")
-ax.set_ylabel("Temps de réponse (en secondes)")
-for tick in ax.get_xticklabels():
-    tick.set_rotation(45)
-
-ax.plot(df2['index'], df2['upstream_rt_mean'], label='Apps')
-ax.plot(df2['index'], df2['static_rt_mean'], label='Statics')
-ax.plot(df2['index'], df2['err_rt_mean'], label='Erreurs')
-
-plt.legend(loc=2)
-fig.savefig('/tmp/rt_requests.png')
+# create dataframe and generate charts
+generate_charts(ranges)
