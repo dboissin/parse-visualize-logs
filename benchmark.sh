6bec7ee1953fda66033e89a8b993e25d7aada70d
dboissin <damien.boissin@web-education.net>
2020-02-16 17:16:10 +0100

    benchmark

---
+++ benchmark.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]
+then
+    echo "Missing log file argument."
+    exit 1
+fi
+
+LOG_FILE=$1
+CLEAN_LOG_FILE=$(echo $LOG_FILE | sed 's|/|_|g')
+RESULT_CSV=out/benchmark_$CLEAN_LOG_FILE.csv
+
+echo 'python,python regex,c,go' > $RESULT_CSV
+
+for i in `seq 0 4`
+do
+    C_TIME=$(/usr/bin/time -f '%e' python3 analyse-logs-c.py $LOG_FILE 2>&1 1>/dev/null)
+    GO_TIME=$(/usr/bin/time -f '%e' python3 analyse-logs-go.py $LOG_FILE 2>&1 1>/dev/null)
+    PY_TIME=$(/usr/bin/time -f '%e' python3 analyse-logs-fullpy.py $LOG_FILE 2>&1 1>/dev/null)
+    PYREGEX_TIME=$(/usr/bin/time -f '%e' python3 analyse-logs-regex.py $LOG_FILE 2>&1 1>/dev/null)
+    echo "$PY_TIME,$PYREGEX_TIME,$C_TIME,$GO_TIME" >> $RESULT_CSV
+done
+
